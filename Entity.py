9ce0aa760f3bee012d2de0c0ab25e23c8ebfbe36
Spieldy <spieldennerjeremy@gmail.com>
2017-03-21 14:05:12 -0400

    function add for simplicty

--- Entity.py
+++ Entity.py
@@ -15,4 +15,27 @@ class Entity(object):
         self.type = entity_type
         self.subtype = entity_subtype
 
+    def is_priority(self):
+        temp = False
+        if self.type == EXIT or self.type == MONSTER or self.type == TRAP or self.type == WALL:
+            temp = True
+        return temp
+
+    def is_spoiled(self):
+        temp = False
+        if self.subtype == TRASH or self.subtype == BONES:
+            temp = True
+        return temp
+
+    def is_type_empty(self):
+        temp = False
+        if self.type == EMPTY:
+            temp = True
+        return temp
+
+    def is_subtype_empty(self):
+        temp = False
+        if self.subtype == EMPTY:
+            temp = True
+        return temp
 
